2082864992f6415114b18173dd2972ae97cf5acb
Cesar <carr_675@hotmail.com>
2019-06-06 10:12:25 -0500

    Se muestran los curriculums de usuarios

---
+++ js/usuario/curriculums.js
@@ -0,0 +1,48 @@
+var app = angular.module('user', ['ngRoute']);
+app.config(function($routeProvider) {
+  $routeProvider.
+  when('/', {
+    templateUrl: './curriculums-usuarios.html',
+    controller: 'myCtrl'
+  }).
+  when('/curriculum/:personId', {
+    templateUrl: 'perfil-usuario.html',
+    controller: 'Ctrl2'
+  });
+});
+
+
+app.controller('myCtrl', function($scope, $location, $http) {
+  $http({
+    url: '/curriculums',
+    method: 'GET',
+  }).then(function successCallback(httpResponse) {
+    if (httpResponse.data.ok) {
+      console.log(httpResponse.data);
+      $scope.cur = httpResponse.data.curriculums;
+    }
+  }, function errorCallback(response) {
+    console.log("fallo", response);
+  });
+  $scope.goTo2 = function(person) {
+    $location.url('/curriculums/' + person.id);
+  };
+});
+
+app.controller('Ctrl2', function($scope, $routeParams, $http) {
+  $http({
+      url: '/req-curr',
+      method: 'POST',
+      data: {
+          id: $routeParams.personId
+      }
+  }).then(function successCallback(httpResponse) {
+      if (httpResponse.data.ok) {
+         console.log(httpResponse.data);
+         $scope.vacantes = httpResponse.data.curriculum;
+      }
+  }, function errorCallback(response) {
+      console.log("fallo", response);
+  });
+  console.log('Pag 2');
+});
